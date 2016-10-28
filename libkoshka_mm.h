c1290ad36b0aff4ab1661233eae6e043858c2813
Clockwork Development Studio <clockworkdevstudio@gmail.com>
2016-10-28 20:58:48 +0100

    Change definition of Image struct so all width/height fields are signed integers. Add fields to Image struct that deal with collision masking.

--- libkoshka_mm.h
+++ libkoshka_mm.h
@@ -1,10 +1,14 @@
 typedef struct
 {
-    unsigned long long int width,height;
+    long long int width,height;
+    long long int width_frames,height_frames;
     long long int handle_x,handle_y;
-    GLuint texture;
+    GLuint *textures;
     SDL_Surface *surface;
-    GLuint mask;
+    GLuint mask_color;
+    unsigned long long int **masks;
+    unsigned long long int mask_width;
+    unsigned long long int mask_height;
 } Image;
 
 void bb_fatal_error(char *msg);
