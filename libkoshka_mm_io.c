6a7652373400e8e989f3e8adb753930a6363d705
Clockwork Development Studio <clockworkdevstudio@gmail.com>
2017-02-07 23:32:12 +0000

    Update copyright year.

--- libkoshka_mm_io.c
+++ libkoshka_mm_io.c
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2014-2016, Clockwork Dev Studio
+Copyright (c) 2014-2017, Clockwork Dev Studio
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
